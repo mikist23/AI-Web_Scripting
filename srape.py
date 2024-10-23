d0ab70d7301705947db53b256a03ca43dbf5c066
@Mikist <mikeochango3@gmail.com>
2024-10-23 14:05:43 +0300

    Loading page resources

--- srape.py
+++ srape.py
@@ -9,4 +9,12 @@ def scrape_website(website):
     options = webdriver.ChromeOptions()
     driver = webdriver.Chrome(Service=Service(chrome_driver_path), options=options)
 
-    
+    try:
+        driver.get(website)
+        print("Pae loaded...")
+        html = driver.page_source
+        time.sleep()
+
+        return html
+    finally:
+        driver.quit()
