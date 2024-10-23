3d27774a6f9e58096b9f464ad41da726a02c73ba
@Mikist <mikeochango3@gmail.com>
2024-10-23 14:50:21 +0300

    initialiazation fo te chromedriver

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from srape import scrape_website
 
 st.title("AI Web 🦜🛠️ Scraper")
 
@@ -6,3 +7,5 @@ url = st.text_input("Enter a Website URL: ")
 
 if st.button("Scrape Site"):
     st.write("Scraping he website")
+    result = scrape_website(url)
+    print(result)
--- srape.py
+++ srape.py
@@ -5,15 +5,15 @@ import time
 def scrape_website(website):
     print("Launching chrome browser...")
 
-    chrome_driver_path = ""
+    chrome_driver_path = "chromedriver.exe"
     options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(Service=Service(chrome_driver_path), options=options)
+    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
 
     try:
         driver.get(website)
         print("Pae loaded...")
         html = driver.page_source
-        time.sleep()
+        time.sleep(10)
 
         return html
     finally:
