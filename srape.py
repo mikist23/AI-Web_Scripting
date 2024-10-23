72d429fcafdb78f1bdc24eac3e96f1e1dbd09c13
@Mikist <mikeochango3@gmail.com>
2024-10-23 14:02:45 +0300

    difine scrape webste functionaliy

--- srape.py
+++ srape.py
@@ -2,3 +2,11 @@ import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
 import time
 
+def scrape_website(website):
+    print("Launching chrome browser...")
+
+    chrome_driver_path = ""
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(Service=Service(chrome_driver_path), options=options)
+
+    
