1716461002191b8612f1ac5bbe8c5b056fe29d96
@Mikist <mikeochango3@gmail.com>
2024-10-25 14:11:36 +0300

    extract body content

--- srape.py
+++ srape.py
@@ -49,3 +49,12 @@ def scrape_website(website):
         html = driver.page_source
         return html
 
+
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    return ""
+
+
