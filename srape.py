ea7d289a9a023abf885d0d00f4dadd71194622d6
@Mikist <mikeochango3@gmail.com>
2024-10-25 14:11:09 +0300

    bright data intergration

--- srape.py
+++ srape.py
@@ -1,21 +1,51 @@
-import selenium.webdriver as webdriver
-from selenium.webdriver.chrome.service import Service
-import time
+# import selenium.webdriver as webdriver
+# from selenium.webdriver.chrome.service import Service
+# import time
+
+
+# def scrape_website(website):
+#     print("Launching chrome browser...")
+
+#     chrome_driver_path = "chromedriver.exe"
+#     options = webdriver.ChromeOptions()
+#     driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
+
+#     try:
+#         driver.get(website)
+#         print("Pae loaded...")
+#         html = driver.page_source
+#         time.sleep(10)
+
+#         return html
+#     finally:
+#         driver.quit()
+
+from selenium.webdriver import Remote, ChromeOptions
+from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SBR_WEBDRIVER = os.getenv("SBR_WEBDRIVER")
 
 
 def scrape_website(website):
-    print("Launching chrome browser...")
-
-    chrome_driver_path = "chromedriver.exe"
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
-
-    try:
+    print("Connecting to Scraping Browser...")
+    sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, "goog", "chrome")
+    with Remote(sbr_connection, options=ChromeOptions()) as driver:
         driver.get(website)
-        print("Pae loaded...")
+        print("Waiting captcha to solve...")
+        solve_res = driver.execute(
+            "executeCdpCommand",
+            {
+                "cmd": "Captcha.waitForSolve",
+                "params": {"detectTimeout": 10000},
+            },
+        )
+        print("Captcha solve status:", solve_res["value"]["status"])
+        print("Navigated! Scraping page content...")
         html = driver.page_source
-        time.sleep(10)
-
         return html
-    finally:
-        driver.quit()
+
